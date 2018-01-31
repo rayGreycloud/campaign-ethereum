f4cb4a29cc429c14ac847b4395c6ba9a5a231fe5
rayGreycloud <rayGreycloud@gmail.com>
2018-01-30 18:20:04 -0800

    Refactor beforeEach; Test deployment of factory and contract

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -14,8 +14,34 @@ let campaign;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
-  
+  // deploy new factory
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledFactory.bytecode })
     .send({ from: accounts[0], gas: '1000000' });
+    
+  factory.setProvider(provider);
+  
+  // create campaign
+  await factory.methods.createCampaign('10000000').send({
+    from: accounts[0],
+    gas: '1000000'
+  });
+  
+  // get first item in deployed campaigns array using destructuring 
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+  // get test instance of campaign 
+  campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress
+  );
+});
+
+describe('Campaigns', () => {
+  it('should deploy a factory and a campaign', () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
+  
+  
 });
