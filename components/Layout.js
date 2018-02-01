3e0e2b0e4658e89552ddc85908ba7fa33e8812f6
rayGreycloud <rayGreycloud@gmail.com>
2018-02-01 15:35:34 -0800

    Move css link tag; add new campaign page

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './Header';
+import Head from 'next/head';
 import { Container } from 'semantic-ui-react';
 
 export default (props) => {
   return (
     <Container>
-      <Header />
+      <Head>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css" />
+      </Head>
+
+      <Header />  
       {props.children}
     </Container>
   );
