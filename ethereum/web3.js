dbb4e11abce3b4577bdbd6bf11f93c0c00061013
rayGreycloud <rayGreycloud@gmail.com>
2018-01-31 17:42:20 -0800

    Refactor to create different web3 instances for server and client

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,14 @@
 import Web3 from 'web3';
 
-// Add check for web3 
+let web3;
 
-const web3 = new Web3(window.web3.currentProvider);
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  const provider = new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+  );
+  web3 = new Web3(provider);
+}
 
 export default web3;
