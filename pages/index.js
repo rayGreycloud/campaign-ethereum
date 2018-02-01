fa5af02eea5e57bd1c94ce58bb2e1c2b0a44a916
rayGreycloud <rayGreycloud@gmail.com>
2018-02-01 13:05:36 -0800

    Wrap campaigns list in Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import react, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
+import Layout from '../components/Layout';
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -23,14 +24,21 @@ class CampaignIndex extends Component {
   
   render() {
     return (
-      <div>
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
-        
-        <div style={{padding: '3rem'}}>
-          <h1>Campaigns</h1>
-          {this.renderCampaigns()}
-        </div>              
-      </div>
+      <Layout>
+        <div>
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+          
+          <div style={{padding: '3rem'}}>
+            <h3>Open Campaigns</h3>
+            {this.renderCampaigns()}
+            <Button 
+              content="Create Campaign"
+              icon="add circle"
+              primary        
+            />
+          </div>              
+        </div>
+      </Layout>
 
     );
   }
