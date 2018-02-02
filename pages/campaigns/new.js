445e42402e2e47a12c1523b9688d46f4305a21ac
rayGreycloud <rayGreycloud@gmail.com>
2018-02-01 18:56:27 -0800

    Add event handler for form submittal

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import { Button, Form, Input } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
   state = {
     minimumContribution: ''
   };
   
+  onSubmit = async (e) => {
+    e.preventDefault();
+    
+    const accounts = await web3.eth.getAccounts();
+    await factory.methods
+      .createCampaign(this.state.minimumContribution)
+      .send({
+        from: accounts[0]
+      });
+  }
+  
   render() {
     return (
       <Layout>
         <h3>Create a new Campaign</h3>
         
-        <Form>
+        <Form onSubmit={this.onSubmit}>
           <Form.Field>
             <label>Set Minimum Contribution</label>
             <Input 
