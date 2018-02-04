d64d01da15200b2cf86df00ece25cf8df3ad2a41
rayGreycloud <rayGreycloud@gmail.com>
2018-02-04 07:20:49 -0800

    Add getDetails to fetch contract info and getRequestsCount

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -88,4 +88,28 @@ contract Campaign {
     request.recipient.transfer(request.value);
     request.complete = true;
   }
+  
+  /* get campaign details */
+  function getDetails() public view returns (
+    uint,
+    uint,
+    uint,
+    uint,
+    address
+  )
+  {
+    return (
+      minimumContribution,
+      this.balance,
+      requests.length,
+      approversCount,
+      manager
+    );
+  }
+  
+  /* get number of requests */
+  function getRequestsCount() public view returns (uint) {
+    return requests.length;
+  }
+  
 }
