c7ef5e41075037c3bbaface540ac49370dcf84bc
rayGreycloud <rayGreycloud@gmail.com>
2018-02-04 07:23:47 -0800

    Refactor to write deployed contract address to file

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -16,22 +16,17 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   
-  console.log('Attempting to deploy from account:', accounts[0]);
+  console.log('Attempting to deploy contract...');
   
   const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledFactory.bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
-  console.log('Contract deployed to:', result.options.address);  
-
-  const deploymentInfo = `
-    Contract deployed from account: ${accounts[0]}
-    To: ${result.options.address}
-  `;
+  console.log('Contract deployed. Saving address file...');  
   
-  fs.writeFile('contractDeploymentInfo.txt', deploymentInfo, (err) => {
+  fs.writeFile('contractDeploymentAddress.txt', result.options.address, (err) => {
     if (err) throw err;
-    console.log('Contract deployment info file has been saved.')
+    console.log('Address file has been saved.');
   });
   
 };
