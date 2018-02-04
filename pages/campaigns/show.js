45a32c2fc2dd2829a972d7149c333f6885df8cc7
rayGreycloud <rayGreycloud@gmail.com>
2018-02-04 06:14:10 -0800

    Scaffold show campaign page

---
+++ pages/campaigns/show.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
+
+
+class CampaignShow extends Component {
+  
+  render() {
+    return (
+      <Layout>
+        <h3>Campaign Details</h3>
+      </Layout>
+
+    );
+  }
+}
+
+export default CampaignShow;
