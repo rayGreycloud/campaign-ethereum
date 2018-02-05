0157fae6a5de491e5c5e770f435d30b2dfd70f1d
rayGreycloud <rayGreycloud@gmail.com>
2018-02-05 10:02:05 -0800

    Add page to show details of specified campaign

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import { Button, Form, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
-import factory from '../../ethereum/factory';
-import web3 from '../../ethereum/web3';
-
+// Using uppercase to avoid collision plus it's quasi-constructor function 
+import Campaign from '../../ethereum/campaign';
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    // Get specific campaign instance
+    const campaign = Campaign(props.query.address);
+    // Returns array-like object
+    const details = await campaign.methods.getDetails().call();
+
+    // Details translation layer
+    return { 
+      minimumContribution: details[0],
+      balance: details[1],
+      requestsCount: details[2],
+      approversCount: details[3],
+      manager: details[4]
+    };
+  }
   
   render() {
     return (
       <Layout>
         <h3>Campaign Details</h3>
+        
+        
       </Layout>
 
     );
