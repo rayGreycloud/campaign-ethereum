7cbce95e55b5106c3156c90cddb2154a84be998f
rayGreycloud <rayGreycloud@gmail.com>
2018-02-05 16:00:51 -0800

    User contribution form

---
+++ components/ContributeForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
+
+class ContributeForm extends Component {
+  render() {
+    return (
+      <Form>
+        <Form.Field>
+          <label>Amount to Contribute</label>
+          <Input
+            label="ether"
+            labelPosition="right"  
+          />
+        </Form.Field>
+        <Button primary>Make Contribution</Button>
+      </Form>
+    );
+  }
+}
+
+export default ContributeForm;
