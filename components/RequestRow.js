59167970cecaf779e547bbd42e6b60ad0f09cf92
rayGreycloud <rayGreycloud@gmail.com>
2018-02-07 18:44:34 -0800

    Display request information and add event handlers for approve and finalize buttons

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,19 +1,58 @@
 import React, { Component } from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
+import web3 from '../ethereum/web3';
+import Campaign from '../ethereum/campaign';
 
-const { Cell, Row } = Table;
+class RequestRow extends Component { 
+  onApprove = async () => {
+    const campaign = Campaign(this.props.address);   
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.approveRequest(this.props.id).send({
+      from: accounts[0]
+    });
+  }
+  
+  onFinalize = async () => {
+    const campaign = Campaign(this.props.address);   
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.finalizeRequest(this.props.id).send({
+      from: accounts[0]
+    });    
+  }
 
-class RequestRow extends Component {
   render() {
+    const { Cell, Row } = Table;
+    const { id, request, approversCount } = this.props;
+    const { description, value, recipient, approvalCount } = request;
+    const readyToFinalize = request.approvalCount > approversCount / 2;   
+    
     return (
-      <Row>
-        <Cell>1234567890ABCDE</Cell>
-        <Cell>Buy Widgets</Cell>
-        <Cell>0.011</Cell>
-        <Cell>BobHashAddress</Cell>
-        <Cell>1</Cell>
-        <Cell>Approve</Cell>
-        <Cell>Finalize</Cell>
+      <Row disabled={request.complete} positive={readyToFinalize && !request.complete}>
+        <Cell>{id}</Cell>
+        <Cell>{description}</Cell>
+        <Cell>{web3.utils.fromWei(value, 'ether')}</Cell>
+        <Cell>{recipient}</Cell>
+        <Cell>{approvalCount}/{approversCount}</Cell>
+        <Cell>
+          {request.complete ? null : (
+            <Button 
+              onClick={this.onApprove}
+              color="green" 
+              basic
+            >Approve
+            </Button>
+          )}
+        </Cell>
+        <Cell>
+          {request.complete ? null : (          
+            <Button 
+              onClick={this.onFinalize}
+              color="teal" 
+              basic
+            >Finalize
+            </Button>
+          )}
+        </Cell>
       </Row>
     );
   }
